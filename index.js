ca8162019b39b9a3ccd5f0d911259603ab9d342a
karen chehade <karen_chehade@hotmail.com>
2021-04-19 08:22:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const farmAnimals = 'cow horse sheep pig chicken';
 
-const [caw,	neigh,bleat,oink,cluck]=farmAnimals.split(' ');
+const [caw,	neigh,bleat,oink,cluck] = farmAnimals.split(' ');
 const [bessie,,dolly, babe,little]=farmAnimals.split(' ');
 const [,blackAndWhite, black,pink,]=farmAnimals.split(' ');
 
